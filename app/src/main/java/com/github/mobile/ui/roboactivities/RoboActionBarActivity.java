df9fc2886d88de81f01c2a012e641e76c3c4b893
Ryan Wagner <rkwagner@ucsd.edu>
2017-02-20 13:57:47 -0800

    Lab 5 initial edits added.

--- app/src/main/java/com/github/mobile/ui/roboactivities/RoboActionBarActivity.java
+++ app/src/main/java/com/github/mobile/ui/roboactivities/RoboActionBarActivity.java
@@ -35,119 +35,120 @@ import roboguice.util.RoboContext;
  * <p>
  * Based on <a href="https://github.com/mccrajs">@mccrajs's</a> implementation <a href="https://github.com/metova/roboguice-appcompat/blob/master/src/com/metova/roboguice/appcompat/RoboActionBarActivity.java">here</a>.
  */
-public class RoboActionBarActivity extends AppCompatActivity implements RoboContext {
+public class RoboActionBarActivity extends RoboActionBarDelegatorActivity {//} implements RoboContext {
 
-    protected EventManager eventManager;
-    protected HashMap<Key<?>, Object> scopedObjects = new HashMap<>();
+protected EventManager eventManager;
+protected HashMap<Key<?>, Object> scopedObjects = new HashMap<>();
 
-    @Override
-    protected void onCreate( Bundle savedInstanceState ) {
+@Override
+public void onCreate( Bundle savedInstanceState ) {
 
-        final RoboInjector injector = RoboGuice.getInjector(this);
-        eventManager = injector.getInstance( EventManager.class );
-        injector.injectMembersWithoutViews( this );
-        super.onCreate( savedInstanceState );
-        eventManager.fire( new OnCreateEvent( savedInstanceState ) );
-    }
+    final RoboInjector injector = RoboGuice.getInjector(this);
+    eventManager = injector.getInstance( EventManager.class );
+    injector.injectMembersWithoutViews( this );
+    super.onCreate( savedInstanceState );
+    eventManager.fire( new OnCreateEvent( savedInstanceState ) );
+}
 
-    @Override
-    public void setContentView(@LayoutRes int layoutResID) {
-        super.setContentView(layoutResID);
+@Override
+public void setContentView(@LayoutRes int layoutResID) {
+    super.setContentView(layoutResID);
 
-        View toolbar = findViewById(R.id.toolbar);
-        if (toolbar != null) {
-            setSupportActionBar((Toolbar) toolbar);
-        }
-    }
-
-    @Override
-    protected void onRestart() {
-
-        super.onRestart();
-        eventManager.fire( new OnRestartEvent() );
-    }
-
-    @Override
-    protected void onStart() {
-
-        super.onStart();
-        eventManager.fire( new OnStartEvent() );
-    }
-
-    @Override
-    protected void onResume() {
-
-        super.onResume();
-        eventManager.fire( new OnResumeEvent() );
-    }
-
-    @Override
-    protected void onPause() {
-
-        super.onPause();
-        eventManager.fire( new OnPauseEvent() );
-    }
-
-    @Override
-    protected void onNewIntent( Intent intent ) {
-
-        super.onNewIntent( intent );
-        eventManager.fire( new OnNewIntentEvent() );
-    }
-
-    @Override
-    protected void onStop() {
-
-        try {
-            eventManager.fire( new OnStopEvent() );
-        }
-        finally {
-            super.onStop();
-        }
-    }
-
-    @Override
-    protected void onDestroy() {
-
-        try {
-            eventManager.fire( new OnDestroyEvent() );
-        }
-        finally {
-            try {
-                RoboGuice.destroyInjector( this );
-            }
-            finally {
-                super.onDestroy();
-            }
-        }
-    }
-
-    @Override
-    public void onConfigurationChanged( Configuration newConfig ) {
-
-        final Configuration currentConfig = getResources().getConfiguration();
-        super.onConfigurationChanged( newConfig );
-        eventManager.fire( new OnConfigurationChangedEvent( currentConfig, newConfig ) );
-    }
-
-    @Override
-    public void onContentChanged() {
-
-        super.onContentChanged();
-        RoboGuice.getInjector( this ).injectViewMembers( this );
-        eventManager.fire( new OnContentChangedEvent() );
-    }
-
-    @Override
-    protected void onActivityResult( int requestCode, int resultCode, Intent data ) {
-
-        super.onActivityResult( requestCode, resultCode, data );
-        eventManager.fire( new OnActivityResultEvent( requestCode, resultCode, data ) );
-    }
-
-    @Override
-    public Map<Key<?>, Object> getScopedObjectMap() {
-
-        return scopedObjects;
+    View toolbar = findViewById(R.id.toolbar);
+    if (toolbar != null) {
+        setSupportActionBar((Toolbar) toolbar);
     }
 }
+
+/*    @Override
+      protected void onRestart() {
+
+      super.onRestart();
+      eventManager.fire( new OnRestartEvent() );
+      }
+
+      @Override
+      protected void onStart() {
+
+      super.onStart();
+      eventManager.fire( new OnStartEvent() );
+      }
+
+      @Override
+      protected void onResume() {
+
+      super.onResume();
+      eventManager.fire( new OnResumeEvent() );
+      }
+
+      @Override
+      protected void onPause() {
+
+      super.onPause();
+      eventManager.fire( new OnPauseEvent() );
+      }
+
+      @Override
+      protected void onNewIntent( Intent intent ) {
+
+      super.onNewIntent( intent );
+      eventManager.fire( new OnNewIntentEvent() );
+      }
+
+      @Override
+      public void onStop() {
+
+      try {
+      eventManager.fire( new OnStopEvent() );
+      }
+      finally {
+      super.onStop();
+      }
+      }
+
+      @Override
+      protected void onDestroy() {
+
+      try {
+      eventManager.fire( new OnDestroyEvent() );
+      }
+      finally {
+      try {
+      RoboGuice.destroyInjector( this );
+      }
+      finally {
+      super.onDestroy();
+      }
+      }
+      }
+
+      @Override
+      public void onConfigurationChanged( Configuration newConfig ) {
+
+      final Configuration currentConfig = getResources().getConfiguration();
+      super.onConfigurationChanged( newConfig );
+      eventManager.fire( new OnConfigurationChangedEvent( currentConfig, newConfig ) );
+      }
+
+      @Override
+public void onContentChanged() {
+
+    super.onContentChanged();
+    RoboGuice.getInjector( this ).injectViewMembers( this );
+    eventManager.fire( new OnContentChangedEvent() );
+}
+
+@Override
+protected void onActivityResult( int requestCode, int resultCode, Intent data ) {
+
+    super.onActivityResult( requestCode, resultCode, data );
+    eventManager.fire( new OnActivityResultEvent( requestCode, resultCode, data ) );
+}
+
+@Override
+public Map<Key<?>, Object> getScopedObjectMap() {
+
+    return scopedObjects;
+}*/
+}
+
