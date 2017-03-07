bdf85cd7a067033aaf3db32f12445ba6ca0a4293
Eric Fakhourian <eric.fakhourian@gmail.com>
2017-03-07 08:08:25 -0800

    Update TraceAspect.java
    Lab8, Our TraceAspect

--- CSE218/ForkHub/app/src/main/java/com/github/mobile/aspectj/TraceAspect.java
+++ CSE218/ForkHub/app/src/main/java/com/github/mobile/aspectj/TraceAspect.java
@@ -1,21 +1,52 @@
-package com.github.mobile.aspectj;
+package com.github.mobile.apectj;
 
+import android.util.Log;
+
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
- * Created by Prateek on 2/12/2017.
+ * Created by ericfa on 3/6/17.
  */
+
 @Aspect
 public class TraceAspect {
-    private static final String POINTCUT_METHOD =
+    private static Logger log = null;
+
+
+    @Pointcut ("call(* *.*(..))")
+    void anyCall() {}
+
+    @AfterThrowing("anyCall()")
+    public void errHandle(JoinPoint jp){
+        Log.d("thisthisthisthis", "thisthisthis");
+
+        if(jp.getThis() != null){
+            log = Logger.getLogger(String.valueOf(jp.getThis().getClass()));
+        }
+        else{
+            log = Logger.getLogger(String.valueOf(TraceAspect.class));
+        }
+        log.log(Level.SEVERE, String.valueOf(jp.getThis().getClass()) + "new exception, details at: ", new Throwable());
+
+
+    }
+    /*private static final String POINTCUT_METHOD =
             "execution(@org.android10.gintonic.annotation.DebugTrace * *(..))";
 
     private static final String POINTCUT_CONSTRUCTOR =
             "execution(@org.android10.gintonic.annotation.DebugTrace *.new(..))";
+
+    private static Logger log = null;
 
     @Pointcut(POINTCUT_METHOD)
     public void methodAnnotatedWithDebugTrace() {}
@@ -24,23 +55,24 @@ public class TraceAspect {
     public void constructorAnnotatedDebugTrace() {}
 
     @Around("methodAnnotatedWithDebugTrace() || constructorAnnotatedDebugTrace()")
-    public Object weaveJoinPoint(ProceedingJoinPoint joinPoint) throws Throwable {
-        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
-        String className = methodSignature.getDeclaringType().getSimpleName();
-        String methodName = methodSignature.getName();
+    public Object weaveJoinPoint(ProceedingJoinPoint joinPoint) throws Throwable{
 
-        final StopWatch stopWatch = new StopWatch();
-        stopWatch.start();
+        Log.d("thisthisthisthis", "thisthisthis");
+
         Object result = joinPoint.proceed();
-        stopWatch.stop();
 
-        DebugLog.log(className, buildLogMessage(methodName, stopWatch.getTotalTimeMillis()));
-
+        if(joinPoint.getThis() != null){
+            log = Logger.getLogger(String.valueOf(joinPoint.getThis().getClass()));
+        }
+        else{
+            log = Logger.getLogger(String.valueOf(TraceAspect.class));
+        }
+        log.log(Level.SEVERE, String.valueOf(joinPoint.getThis().getClass()) + "new exception, details at: ", new Throwable());
         return result;
     }
     private static String buildLogMessage(String methodName, long methodDuration) {
         StringBuilder message = new StringBuilder();
-        message.append("Gintonic --> ");
+        message.append("ForkHub --> ");
         message.append(methodName);
         message.append(" --> ");
         message.append("[");
@@ -49,5 +81,5 @@ public class TraceAspect {
         message.append("]");
 
         return message.toString();
-    }
+    }*/
 }
